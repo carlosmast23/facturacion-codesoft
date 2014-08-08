897e357011b167d345886178cde9bc9bf5dad28f
carlosmast2302@gmail.com <carlosmast2302@gmail.com@cbcb600c-ff66-99bd-437b-6f4799686b69>
2014-08-08 20:34:40 +0000

    vercion actualizada

---
+++ trunk/FacturacionCodesoft/src/facturacioncodesoft/FacturacionCodesoft.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package facturacioncodesoft;
+
+/**
+ *
+ * @author carlos
+ */
+public class FacturacionCodesoft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
